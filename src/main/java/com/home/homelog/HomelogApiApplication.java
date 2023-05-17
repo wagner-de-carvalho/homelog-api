bab75b164b8e7fa07e107d00bb53302d740459dc
wagner <wagncarv@outlook.com>
2023-05-17 09:43:38 -0300

    first commit

---
+++ src/main/java/com/home/homelog/HomelogApiApplication.java
@@ -0,0 +1,13 @@
+package com.home.homelog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomelogApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HomelogApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/home/homelog/HomelogApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.home.homelog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HomelogApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
